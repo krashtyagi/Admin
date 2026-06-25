3d2e4c16351a4553ab40d9470ea36657f03c5322
mohit-rajput-raj <rajputmohit19101169@gmail.com>
2026-06-25 23:06:22 +0530

    type-change-password-2506

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -8,11 +8,11 @@ export const LoginScshema = z.object({
     .min(8, { message: "Your password must be atleast 8 characters long" })
     .max(64, {
       message: "Your password can not be longer then 64 characters long",
-    })
-    .refine(
-      (value) => /^[a-zA-Z0-9_.-]*$/.test(value ?? ""),
-      "password should contain only alphabets and numbers",
-    ),
+    }),
+  // .refine(
+  //   (value) => /^[a-zA-Z0-9_.-]*$/.test(value ?? ""),
+  //   "password should contain only alphabets and numbers",
+  // ),
 });
 export type LoginFormProps = z.infer<typeof LoginScshema>;
 
@@ -23,11 +23,11 @@ export const ResetPasswordSchema = z.object({
     .min(8, { message: "Your password must be atleast 8 characters long" })
     .max(64, {
       message: "Your password can not be longer then 64 characters long",
-    })
-    .refine(
-      (value) => /^[a-zA-Z0-9_.-]*$/.test(value ?? ""),
-      "password should contain only alphabets and numbers",
-    ),
+    }),
+  // .refine(
+  //   (value) => /^[a-zA-Z0-9_.-]*$/.test(value ?? ""),
+  //   "password should contain only alphabets and numbers",
+  // ),
   confirmPassword: z
     .string()
     .min(8, { message: "Your password must be atleast 8 characters long" }),
